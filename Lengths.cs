4c35acedd563a4811c2e43e24ec2a48dbeb53abb
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-06-13 11:50:32 -0700

    Initial commit.

---
+++ Lengths.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitConversions
+{
+    public class Lengths
+    {
+        public static double FeettoMeters(int feet)
+        {
+            double meters = feet / 3.2808;
+
+            return Math.Round(meters, 2);
+        }
+
+        public static double MeterstoFeet(int meters)
+        {
+            double feet = meters * 3.2808;
+
+            return Math.Round(feet, 2);
+        }
+
+        public static double MilestoKilos(int miles)
+        {
+            double kilos = miles / 0.62137;
+
+            return Math.Round(kilos, 2);
+        }
+
+        public static double KilostoMiles(int kilos)
+        {
+            double miles = kilos * 0.62137;
+
+            return Math.Round(miles, 2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UnitConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
