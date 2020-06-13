f9ff43987a006d42f833097d858a9f3f4263e747
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-06-13 16:11:56 -0700

    Added lengths conversions.

--- Lengths.cs
+++ Lengths.cs
@@ -33,5 +33,45 @@ namespace UnitConversions
 
             return Math.Round(miles, 2);
         }
+
+        // Present the user w/ their choices, and prompt them to make a selection
+        public static void LengthChoices()
+        {
+            // Define user options in a dictionary
+            Dictionary<int, string> userOptions = new Dictionary<int, string>();
+
+            userOptions.Add(1, "Feet to Meters.");
+            userOptions.Add(2, "Meters to Feet.");
+            userOptions.Add(3, "Miles to Kilos.");
+            userOptions.Add(4, "Kilos to Miles.");
+
+            // Blank line for readability
+            Console.WriteLine();
+
+            foreach (var keyval in userOptions)
+            {
+                Console.WriteLine(keyval);
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Please enter an int based on the options above: ");
+            // Prompt the the user to enter a choice
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+            
+
+            if (userChoice == 1)
+            {
+               Console.Write("Enter a number of feet to convert to meters: "); 
+               int userFeet = Convert.ToInt32(Console.ReadLine());
+               Console.WriteLine($"{userFeet} feet equals {Lengths.MeterstoFeet(userFeet)} meters.");
+            }
+            else if (userChoice == 2)
+            {
+                Console.Write("Enter a number of meters to convert to feet: "); 
+                int userMeters = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"{userMeters} meter(s) equals {Lengths.MeterstoFeet(userMeters)} feet.");
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace UnitConversions
 {
     class Program
     {
+        static void UserOptions()
+        {
+            // Define user options in a dictionary
+            Dictionary<int, string> userOptions = new Dictionary<int, string>();
+
+            userOptions.Add(1, "Feet to Meters.");
+            userOptions.Add(2, "Meters to Feet.");
+            userOptions.Add(3, "Miles to Kilos.");
+            userOptions.Add(4, "Kilos to Miles.");
+
+            // Blank line for readability
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Write("Press any key to exit.");
-            Console.ReadKey();
+            Lengths.LengthChoices();
         }
     }
 }
