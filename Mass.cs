466347dfd6a897c852f216bfb27c78e0555010a9
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-06-14 14:07:20 -0700

    Misc changes.

--- Mass.cs
+++ Mass.cs
@@ -26,5 +26,54 @@ namespace UnitConversions
 
             return Math.Round(grams, 4);
         }
+
+        public static double GramstoOunces(int grams)
+        {
+            double ounces = grams * 0.035274;
+
+            return Math.Round(ounces, 4);
+        }
+
+        public static void MassChoices()
+        {
+            // Define user options in a dictionary
+            Dictionary<int, string> userOptions = new Dictionary<int, string>();
+
+            userOptions.Add(0, "Main Menu.");
+            userOptions.Add(1, "Pounds to Kilograms.");
+            userOptions.Add(2, "Kilograms to Pounds.");
+            userOptions.Add(3, "Ounces to Grams.");
+            userOptions.Add(4, "Grams to Ounces.");
+
+            // Blank line for readability
+            Console.WriteLine();
+
+            foreach (var keyval in userOptions)
+            {
+                Console.WriteLine(keyval);
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Please enter an int based on the options above: ");
+            // Prompt the the user to enter a choice
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+
+            if (userChoice == 0)
+            {
+                Program.Main(null);
+            }
+            else if (userChoice == 1)
+            {
+                Console.Write("Enter a number of pounds to convert to kilograms: "); 
+                int userLbs = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"{userLbs} feet equals {Mass.PoundstoKilos(userLbs)} meters.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please enter a valid int.");
+                Mass.MassChoices();
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,25 +3,49 @@ using System.Collections.Generic;
 
 namespace UnitConversions
 {
-    class Program
+    public class Program
     {
         static void UserOptions()
         {
             // Define user options in a dictionary
             Dictionary<int, string> userOptions = new Dictionary<int, string>();
 
-            userOptions.Add(1, "Feet to Meters.");
-            userOptions.Add(2, "Meters to Feet.");
-            userOptions.Add(3, "Miles to Kilos.");
-            userOptions.Add(4, "Kilos to Miles.");
+            userOptions.Add(1, "Length Conversions.");
+            userOptions.Add(2, "Mass Conversions.");
+
+            // Blank line for readability
+            Console.WriteLine();
+
+            foreach (var keyval in userOptions)
+            {
+                Console.WriteLine(keyval);
+            }
 
             // Blank line for readability
             Console.WriteLine();
         }
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Lengths.LengthChoices();
+            UserOptions();
+
+            Console.Write("Please enter an int based on the options above: ");
+            // Prompt the the user to enter a choice
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+
+            if (userChoice == 1)
+            {
+                Lengths.LengthChoices();
+            }
+            else if (userChoice == 2)
+            {
+                Mass.MassChoices();
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please enter a valid int.");
+                UserOptions();
+            }
         }
     }
 }
