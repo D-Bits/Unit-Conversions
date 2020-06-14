de3e3ad8671239539c6fa60d768c116a5cc42d4c
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-06-14 14:07:11 -0700

    Returns to main after calculation is complete.

--- Lengths.cs
+++ Lengths.cs
@@ -40,6 +40,7 @@ namespace UnitConversions
             // Define user options in a dictionary
             Dictionary<int, string> userOptions = new Dictionary<int, string>();
 
+            userOptions.Add(0, "Main Menu.");
             userOptions.Add(1, "Feet to Meters.");
             userOptions.Add(2, "Meters to Feet.");
             userOptions.Add(3, "Miles to Kilos.");
@@ -59,30 +60,42 @@ namespace UnitConversions
             // Prompt the the user to enter a choice
             int userChoice = Convert.ToInt32(Console.ReadLine());
             
-
-            if (userChoice == 1)
+            if (userChoice == 0)
+            {
+                Program.Main(null);
+            }
+            else if (userChoice == 1)
             {
                Console.Write("Enter a number of feet to convert to meters: "); 
                int userFeet = Convert.ToInt32(Console.ReadLine());
                Console.WriteLine($"{userFeet} feet equals {Lengths.FeettoMeters(userFeet)} meters.");
+               Program.Main(null);
             }
             else if (userChoice == 2)
             {
                 Console.Write("Enter a number of meters to convert to feet: "); 
                 int userMeters = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine($"{userMeters} meter(s) equals {Lengths.MeterstoFeet(userMeters)} feet.");
+                Program.Main(null);
             }
             else if (userChoice == 3)
             {
                 Console.Write("Enter a number of miles to convert to kilometers: "); 
                 int userMiles = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine($"{userMiles} mile(s) equals {Lengths.MilestoKilos(userMiles)} kilometers.");
+                Program.Main(null);
             }
             else if (userChoice == 4)
             {
                 Console.Write("Enter a number of kilometers to convert to miles: ");
                 int userKilos = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine($"{userKilos} kilometers(s) equals {Lengths.MilestoKilos(userKilos)} mile(s).");
+                Program.Main(null);
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please enter a valid int.");
+                Lengths.LengthChoices();
             }
         }
     }
