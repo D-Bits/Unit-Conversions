f85c09f13917c7bef3c8da5cdc7cfa97d11782ed
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-06-17 16:42:40 -0700

    Misc changes.

--- Lengths.cs
+++ Lengths.cs
@@ -6,28 +6,28 @@ namespace UnitConversions
 {
     public class Lengths
     {
-        static double FeettoMeters(int feet)
+        static double FeettoMeters(double feet)
         {
             double meters = feet / 3.2808;
 
             return Math.Round(meters, 4);
         }
 
-        static double MeterstoFeet(int meters)
+        static double MeterstoFeet(double meters)
         {
             double feet = meters * 3.2808;
 
             return Math.Round(feet, 4);
         }
 
-        static double MilestoKilos(int miles)
+        static double MilestoKilos(double miles)
         {
             double kilos = miles / 0.62137;
 
             return Math.Round(kilos, 4);
         }
 
-        static double KilostoMiles(int kilos)
+        static double KilostoMiles(double kilos)
         {
             double miles = kilos * 0.62137;
 
@@ -58,7 +58,7 @@ namespace UnitConversions
 
             Console.Write("Please enter an int based on the options above: ");
             // Prompt the the user to enter a choice
-            int userChoice = Convert.ToInt32(Console.ReadLine());
+            double userChoice = Convert.ToDouble(Console.ReadLine());
             
             if (userChoice == 0)
             {
@@ -67,35 +67,35 @@ namespace UnitConversions
             else if (userChoice == 1)
             {
                 Console.Write("Enter a number of feet to convert to meters: "); 
-                int userFeet = Convert.ToInt32(Console.ReadLine());
+                double userFeet = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine($"{userFeet} feet equals {Lengths.FeettoMeters(userFeet)} meters.");
                 Lengths.LengthChoices();
             }
             else if (userChoice == 2)
             {
                 Console.Write("Enter a number of meters to convert to feet: "); 
-                int userMeters = Convert.ToInt32(Console.ReadLine());
+                double userMeters = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine($"{userMeters} meter(s) equals {Lengths.MeterstoFeet(userMeters)} feet.");
                 Program.Main(null);
             }
             else if (userChoice == 3)
             {
                 Console.Write("Enter a number of miles to convert to kilometers: "); 
-                int userMiles = Convert.ToInt32(Console.ReadLine());
+                double userMiles = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine($"{userMiles} mile(s) equals {Lengths.MilestoKilos(userMiles)} kilometers.");
                 Program.Main(null);
             }
             else if (userChoice == 4)
             {
                 Console.Write("Enter a number of kilometers to convert to miles: ");
-                int userKilos = Convert.ToInt32(Console.ReadLine());
+                double userKilos = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine($"{userKilos} kilometers(s) equals {Lengths.MilestoKilos(userKilos)} mile(s).");
                 Program.Main(null);
             }
             else
             {
                 Console.WriteLine();
-                Console.WriteLine("Invalid choice. Please enter a valid int.");
+                Console.WriteLine("***Invalid choice. Please enter a valid int.***");
                 Lengths.LengthChoices();
             }
         }
