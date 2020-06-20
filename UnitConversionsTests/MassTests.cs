d343bd052c0f7733dff0a00c5a3e76180c8899bc
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-06-20 16:34:06 -0700

    Added MS Test support.

---
+++ UnitConversionsTests/MassTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitConversionsTests
+{
+    [TestClass]
+    public class UnitConversionsTests
+    {
+        [TestMethod]
+        public void TestPoundstoKilos()
+        {
+
+        }
+    }
+}
